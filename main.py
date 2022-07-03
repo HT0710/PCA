a25ee950d546b5a1e37c8af80db004f026a17a65
ArtA <pthung7102002@gmail.com>
2022-07-03 21:22:22 +0700

    3/7

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from data import *
 
 filename = "Wage_data.csv"
-raw_data = load_data(filename)
+raw_data = load_data(filename).to_numpy()
 pca_data = pca_data(filename)
 
-print(raw_data.values)
-print(pca_data)
+print(raw_data.shape)
+print(pca_data.shape)
